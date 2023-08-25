cbb2c9ab0ad83e760605ea363af1f04dd81b906a
thcabrera <thiagomartincabreralavezzi@gmail.com>
2023-08-25 20:10:16 -0300

    tests with unittest

--- test_simulador_de_una_cola.py
+++ test_simulador_de_una_cola.py
@@ -1,11 +1,16 @@
 from mock_simulador_de_una_cola import MockSimuladorDeUnaCola
+import unittest
 
-simulation = MockSimuladorDeUnaCola(
-    ptlls = [10,5,60,15,30,35,10,20,15],
-    ptss = [40,5,15,10,30,45,15,20,10],
-    tf = 150)
-simulation.start()
-if ("%.2f" % simulation.pps) == "32.86" and ("%.2f" % simulation.pto) == "11.11":
-    print("Test superado!")
-else:
-    print(simulation.pps, simulation.pto)
+class TestModels(unittest.TestCase):
+
+    def test_upper(self):
+        simulation = MockSimuladorDeUnaCola(
+        ptlls = [10,5,60,15,30,35,10,20,15],
+        ptss = [40,5,15,10,30,45,15,20,10],
+        tf = 150)
+        simulation.start()
+        self.assertEqual(("%.2f" % simulation.pps), "32.86", "Falló el calculo del PPS!")
+        self.assertEqual(("%.2f" % simulation.pto), "11.11", "Falló el cálculo del PTO!")
+
+if __name__ == '__main__':
+    unittest.main()
